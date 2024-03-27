3caf5cd0f63f82b917ebedec9debf86c2b8fc221
Antonio Ordóñez Molina <aom387@inlumine.ual.es>
2024-03-27 23:07:48 +0100

    Prueba Vaadin doble selección.

--- ordonezruchelli/frontend/src/vista-anadirnoticiaaseccion.ts
+++ ordonezruchelli/frontend/src/vista-anadirnoticiaaseccion.ts
@@ -1,4 +1,11 @@
 import { LitElement, html, css, customElement } from 'lit-element';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-horizontal-layout.js';
+import '@vaadin/vaadin-select/src/vaadin-select.js';
+import '@vaadin/vaadin-list-box/src/vaadin-list-box.js';
+import '@vaadin/vaadin-item/src/vaadin-item.js';
+import './vista-listadenoticiasquenoestanenlaseccion';
+import './vista-listadodeseccionesgenerico';
+import '@vaadin/vaadin-button/src/vaadin-button.js';
 
 @customElement('vista-anadirnoticiaaseccion')
 export class VistaAnadirnoticiaaseccion extends LitElement {
@@ -12,7 +19,45 @@ export class VistaAnadirnoticiaaseccion extends LitElement {
   }
 
   render() {
-    return html``;
+    return html`
+<vaadin-horizontal-layout class="content" style="width: 100%; height: 100%;">
+ <vaadin-select value="Item one">
+  <template>
+   <vaadin-list-box selected="0">
+    <vaadin-item selected>
+      Item one 
+    </vaadin-item>
+    <vaadin-item>
+      Item two 
+    </vaadin-item>
+    <vaadin-item>
+      Item three 
+    </vaadin-item>
+    <vista-listadenoticiasquenoestanenlaseccion></vista-listadenoticiasquenoestanenlaseccion>
+   </vaadin-list-box>
+  </template>
+ </vaadin-select>
+ <vaadin-select value="Item one">
+  <template>
+   <vaadin-list-box>
+    <vaadin-item>
+      Item one 
+    </vaadin-item>
+    <vaadin-item>
+      Item two 
+    </vaadin-item>
+    <vaadin-item>
+      Item three 
+    </vaadin-item>
+   </vaadin-list-box>
+  </template>
+  <vista-listadodeseccionesgenerico></vista-listadodeseccionesgenerico>
+ </vaadin-select>
+ <vaadin-button id="anadirNoticiaSeccion">
+  Añadir
+ </vaadin-button>
+</vaadin-horizontal-layout>
+`;
   }
 
   // Remove this method to render the contents of this view inside Shadow DOM
--- ordonezruchelli/src/main/java/vistas/VistaAnadirnoticiaaseccion.java
+++ ordonezruchelli/src/main/java/vistas/VistaAnadirnoticiaaseccion.java
@@ -3,12 +3,17 @@ package vistas;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.littemplate.LitTemplate;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.template.Id;
 
 @Tag("vista-anadirnoticiaaseccion")
 @JsModule("./src/vista-anadirnoticiaaseccion.ts")
 public class VistaAnadirnoticiaaseccion extends LitTemplate {
 
-    public VistaAnadirnoticiaaseccion() {
+    @Id("anadirNoticiaSeccion")
+	private Button anadirNoticiaSeccion;
+
+	public VistaAnadirnoticiaaseccion() {
         // You can initialise any data required for the connected UI components here.
     }
 
